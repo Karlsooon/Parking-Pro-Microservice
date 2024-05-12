170c88f81319857fc49971f5e88d13b89dcd4911
Karlsooon <karakatalmasova2@gmail.com>
2024-05-12 14:46:43 +0500

    error 8080

--- PARKING-SERVICE/src/main/java/com/almasova/googlePlacesAPI/GooglePlacesApiServerApplication.java
+++ PARKING-SERVICE/src/main/java/com/almasova/googlePlacesAPI/GooglePlacesApiServerApplication.java
@@ -2,11 +2,19 @@ package com.almasova.googlePlacesAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GooglePlacesApiServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GooglePlacesApiServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GooglePlacesApiServerApplication.class, args);
+    }
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
