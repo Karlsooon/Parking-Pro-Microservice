de44e638c882ad2c8435c2deb31f86202acdff99
Karlsooon <karakatalmasova2@gmail.com>
2024-05-13 21:07:55 +0500

    added utils module

---
+++ parking-pro-utils/src/main/java/com/example/parkingproutils/ParkingProUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkingproutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingProUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingProUtilsApplication.class, args);
+	}
+
+}
---
+++ parking-pro-utils/src/test/java/com/example/parkingproutils/ParkingProUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkingproutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingProUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
